96babd8d757667150b031949bc0987581f38c179
Vincent Son <107471539+szw0407@users.noreply.github.com>
2024-08-20 02:00:30 +0800

    add canteen operations

---
+++ src/canteen.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, Depends
+from fastapi import HTTPException
+
+from sql_app.crud import canteen
+from sql_app.schemas import CanteenItem, CanteenBase
+from sql_app import get_db
+
+router = APIRouter()
+@router.get("/all", response_model=list[CanteenItem])
+def get_all_canteen(db = Depends(get_db)):
+    return canteen.get_all(db)
+@router.get("/{canteen_id}", response_model=CanteenItem)
+def get_canteen(canteen_id: int, db = Depends(get_db)):
+    return canteen.get_info(db, canteen_id)
+
+@router.get("/campus/{campus_name}",)
+def get_canteen_by_campus(campus_name: str, db = Depends(get_db)):
+    return canteen.get_by_campus(db, campus_name)
+
+@router.post("", response_model=CanteenItem)
+def add_canteen(data: CanteenBase, db = Depends(get_db)):
+    return canteen.add(db, data)
+
+@router.delete("/{canteen_id}", response_model=dict[str, str])
+def delete_canteen(canteen_id: int, db = Depends(get_db)):
+    return canteen.delete(db, canteen_id)
+
+@router.put("", response_model=dict[str, str])
+def update_canteen(data: CanteenItem, db = Depends(get_db)):
+    try:
+        return canteen.update(db, data)
+    except:
+        raise HTTPException(status_code=400, detail="Invalid canteen id")
---
+++ src/sql_app/crud/canteen.py
@@ -0,0 +1,38 @@
+from sqlalchemy.orm import Session
+
+from ..models import Canteen
+from ..schemas import CanteenBase, CanteenItem
+
+def get_info(db: Session, canteen_id: int) -> Canteen | None:
+    return db.query(Canteen).filter(Canteen.id == canteen_id).first()
+
+def get_all(db: Session, skip: int = 0, limit: int = 200) -> list[Canteen]:
+    return db.query(Canteen).offset(skip).limit(limit).all()
+
+def add(db: Session, canteen: CanteenBase) -> Canteen:
+    db_canteen = Canteen(name=canteen.name, 
+                          description=canteen.description,
+                          image=canteen.image, 
+                          campus=canteen.campus)
+    db.add(db_canteen)
+    db.commit()
+    db.refresh(db_canteen)
+    return db_canteen
+
+def delete(db: Session, canteen_id: int) -> dict[str, str]:
+    db.query(Canteen).filter(Canteen.id == canteen_id).delete()
+    db.commit()
+    return {"detail": "Delete Success"}
+
+def update(db: Session, data: CanteenItem) -> dict[str, str]:
+    db_canteen: Canteen | None = db.query(Canteen).filter(Canteen.id == data.id).first() if data.id else None
+    if db_canteen is None:
+        raise Exception("No such canteen")
+    db_canteen.name = data.name
+    db_canteen.description = data.description
+    db_canteen.image = data.image if data.image else db_canteen.image
+    db.commit()
+    return {"detail": "Update Success"}
+
+def get_by_campus(db: Session, campus_name: str) -> list[Canteen]:
+    return db.query(Canteen).filter(Canteen.campus == campus_name).all()
