15c78a857fdc47563ba19ecad1f889483fe70012
Vincent Son <107471539+szw0407@users.noreply.github.com>
2024-08-26 01:28:03 +0800

    chore: Update get function in new_dish.py to include filtering by dish_id in NewDish table

--- src/sql_app/crud/new_dish.py
+++ src/sql_app/crud/new_dish.py
@@ -1,20 +1,22 @@
+from typing import List, Tuple
+from sqlalchemy.engine.row import Row
 from sqlalchemy.orm import Session
 
 from ..models import Dish, NewDish
 
 
 def get(db: Session, canteen: int):
-    return db.query(Dish).filter(Dish.canteen == canteen).all()
+    # return Dish whose canteen is canteen and its id in table NewDish
+    return db.query(Dish).filter(Dish.canteen == canteen, Dish.id == NewDish.dish_id).all()
 
 def delete(db: Session, dish_id: int):
-    db.query(Dish).filter(Dish.id == dish_id).delete()
+    db.query(NewDish).filter(NewDish.dish_id == dish_id).delete()
     db.commit()
 
 def add(db: Session, dish_id: int) -> Dish | None:
-    db_dish = db.query(Dish).filter(Dish.id == dish_id).first()
+    db_dish: Dish | None = db.query(Dish).filter(Dish.id == dish_id).first()
     if db_dish:
         db.add(NewDish(
             dish_id=id
         ))
-        return db_dish
-
+    return db_dish
