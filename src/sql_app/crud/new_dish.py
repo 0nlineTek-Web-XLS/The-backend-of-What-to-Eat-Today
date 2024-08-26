84ee144c58bd416f6710c476835c94511f84e5e3
Vincent Son <107471539+szw0407@users.noreply.github.com>
2024-08-26 16:38:37 +0800

    chore: Update new_dish CRUD operations and optimize query performance

--- src/sql_app/crud/new_dish.py
+++ src/sql_app/crud/new_dish.py
@@ -7,16 +7,21 @@ from ..models import Dish, NewDish
 
 def get(db: Session, canteen: int):
     # return Dish whose canteen is canteen and its id in table NewDish
-    return db.query(Dish).filter(Dish.canteen == canteen, Dish.id == NewDish.dish_id).all()
+    return db.query(Dish).join(NewDish, Dish.id == NewDish.dish_id).filter(Dish.canteen == canteen).all()
 
 def delete(db: Session, dish_id: int):
     db.query(NewDish).filter(NewDish.dish_id == dish_id).delete()
     db.commit()
+    return {
+        "status": "success"
+    }
 
 def add(db: Session, dish_id: int) -> Dish | None:
     db_dish: Dish | None = db.query(Dish).filter(Dish.id == dish_id).first()
     if db_dish:
         db.add(NewDish(
-            dish_id=id
+            dish_id=dish_id
         ))
+    db.commit()
+    db.refresh(db_dish)
     return db_dish
