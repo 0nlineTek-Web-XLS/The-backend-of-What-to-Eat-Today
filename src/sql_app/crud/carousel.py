0ed5ae29ab218a731df7b9c4b3e05ddb4bcda6b9
Vincent Son <107471539+szw0407@users.noreply.github.com>
2024-10-17 00:59:51 +0800

    Refactor add and get functions in carousel CRUD module

--- src/sql_app/crud/carousel.py
+++ src/sql_app/crud/carousel.py
@@ -3,19 +3,17 @@ from sqlalchemy.orm import Session
 from ..models import Carousel
 from ..schemas import CarouselItem
 
-def add(db: Session, carousel: CarouselItem):
-    db_carousel = Carousel(canteen=carousel.canteen, 
+def add(db: Session, carousel: CarouselItem) -> Carousel:
+    db_carousel = Carousel(canteen=carousel.canteen,
                           image=carousel.image)
     db.add(db_carousel)
     db.commit()
     db.refresh(db_carousel)
     return db_carousel
 
-def get(db: Session, canteen: int):
+def get(db: Session, canteen: int) -> list[Carousel]:
     return db.query(Carousel).filter(Carousel.canteen == canteen).all()
 
 def delete(db: Session, cid: int):
     db.query(Carousel).filter(Carousel.id == cid).delete()
     db.commit()
-
-
