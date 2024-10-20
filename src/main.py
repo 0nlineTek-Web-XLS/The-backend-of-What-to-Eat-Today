96fa1976c20ee51e6ad718f1f3c9136342cb8088
Vincent Son <107471539+szw0407@users.noreply.github.com>
2024-10-21 00:52:12 +0800

    Refactor main.py and add object storage module

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ import marks
 import feedback
 import uvicorn
 import comments
-
+import obj_storage
 app = FastAPI()
 
 # add CORS
@@ -37,7 +37,11 @@ app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(comments.router, prefix="/comments", tags=["comments"])
 app.include_router(marks.router, prefix="/marks", tags=["marks"])
 app.include_router(feedback.router, prefix="/feedback", tags=["feedback"])
+app.include_router(obj_storage.router, prefix="/static", tags=["storage"])
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
--- src/obj_storage/router.py
+++ src/obj_storage/router.py
@@ -1,3 +1,33 @@
-from fastapi import APIRouter
+from io import BytesIO
+from typing import Annotated
+from fastapi import APIRouter, File, UploadFile
+from fastapi.responses import StreamingResponse
+import uuid
 
 router = APIRouter()
+
+from .client import minio_client, bucket_name
+
+@router.get("/list")
+async def list_objects():
+    """
+    List all objects in the given bucket
+    """
+    return minio_client.list_objects(bucket_name)
+
+@router.get("/{object_name}/info")
+async def get_object_data(object_name: str):
+    return minio_client.stat_object(bucket_name, object_name)
+
+# @router.get("/get/{object_name}")
+# async def get_object(object_name: str):
+#     # return minio_client.get_object(bucket_name, object_name)
+#     return StreamingResponse(minio_client.get_object(bucket_name, object_name))
+
+@router.post("/upload/")
+async def upload_object(file: Annotated[bytes, File()]):
+    object_name = str(uuid.uuid4())
+    minio_client.put_object(bucket_name, object_name, BytesIO(file), len(file))
+    return {"object_name": object_name}
+
+        
